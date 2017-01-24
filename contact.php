f41475443153d98f438b8decffb7d551eef7970e
niallcs <syfer@skynet.ie>
2017-01-24 19:55:40 +0000

    Added nameTxt form field in contact.php

--- contact.php
+++ contact.php
@@ -6,6 +6,7 @@
 	<body>
 		<h1>Contact Us</h1>
 		<form>
+			<input type="text" name="nameTxt" placeholder="Your name" />
 			<input type="text" name="emailTxt" placeholder="Your email address" />
 			<input type="submit" value="send" />
 		</form>
