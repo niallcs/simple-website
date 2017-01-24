f8edcf2d0b21efc4ef3b405f993f3f03db7f3510
niallcs <syfer@skynet.ie>
2017-01-24 19:53:06 +0000

    Added initial contact.php webpage

---
+++ contact.php
@@ -0,0 +1,13 @@
+<html>
+	
+	<head>
+		<title>Contact Us</title>
+	</head>
+	<body>
+		<h1>Contact Us</h1>
+		<form>
+			<input type="text" name="emailTxt" placeholder="Your email address" />
+			<input type="submit" value="send" />
+		</form>
+	</body>
+</html>
